0f29dc25355029fa5cc8441509c271ecce3424a5
radoslawprzychoda <radoslaw_przychoda@MacBook-Pro.lan>
2020-06-13 11:08:06 +0200

    Fixes from the code review

--- tasks/Module 2/task_1.py
+++ tasks/Module 2/task_1.py
@@ -1,36 +1,45 @@
+from collections import defaultdict
+
+ITEMS_TO_SKIP = ['chewed gum', 'rubbish', 'used tissue']
+MAX_INVENTORY_CAP = 80
+HEAVY_WEIGHT_THRESHOLD = 70
+LIGHT_WEIGHT_THRESHOLD = 60
+
+
 def display_inventory(inventory):
+    '''
+    Displays the contents of the inventory
+    '''
     print("Inventory:")
     item_total = 0
     for item_name, item_count in inventory.items():
         print(item_count, item_name)
         item_total += item_count
-    print("Total number of items: " + str(item_total))
-    if item_total >= 80:
+    print(f"Total number of items: {item_total}")
+    if item_total >= MAX_INVENTORY_CAP:
         print("CAUTION: You are overloaded, can't move!")
-    elif item_total >= 70:
+    elif item_total >= HEAVY_WEIGHT_THRESHOLD:
         print("CAUTION: Your equipment is very heavy, you're moving slower than usual!")
-    elif item_total >= 60:
+    elif item_total >= LIGHT_WEIGHT_THRESHOLD:
         print("CAUTION: Your backpack weighs a lot, your stamina runs out quicker!")
 
 
-def add_to_inventory(inventory, added_items):
-    skipped = {}
+def add_to_inventory(inventory, added_items, items_to_skip):
+    '''
+    Ads useful items to the inventory and skips not useful.
+    Returns a new inventory with added items.
+    '''
+    skipped = defaultdict(int)
     added_count = 0
     for item_name in added_items:
-        if item_name in ['chewed gum', 'rubbish', 'used tissue']:
-            if item_name in skipped.keys():
-                skipped[item_name] += 1
-            else:
-                skipped[item_name] = 1;
+        if item_name in items_to_skip:
+            skipped[item_name] += 1
         else:
-            if item_name in inventory.keys():
-                inventory[item_name] += 1
-            else:
-                inventory[item_name] = 1
+            inventory[item_name] += 1
             added_count += 1
-    print("Added " + str(added_count) + " items to the inventory")
+    print(f"Added {added_count} items to the inventory")
     # print skipped if any
-    if skipped.items():
+    if skipped:
         print("Skipped:")
         for skipped_item_name, skipped_item_count in skipped.items():
             print(skipped_item_count, skipped_item_name)
@@ -38,9 +47,9 @@ def add_to_inventory(inventory, added_items):
 
 
 inv = {'gold coin': 42, 'rope': 1}
-dragonLoot = [
+inv_defdict = defaultdict(int, inv)
+dragon_loot = [
     'gold coin', 'chewed gum', 'dagger', 'gold coin', 'gold coin', 'ruby',
     'rubbish', 'chewed gum', 'used tissue', 'rope', 'rope']
-inv = add_to_inventory(inv, dragonLoot)
+inv = add_to_inventory(inv_defdict, dragon_loot, ITEMS_TO_SKIP)
 display_inventory(inv)
-
